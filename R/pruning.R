f8732de5a92f1eea4433646cd0bd5290b25e3607
Jaroslav Kuchař <jaroslav.kuchar@gmail.com>
2015-05-19 10:36:07 +0200

    rCBA release

---
+++ R/pruning.R
@@ -0,0 +1,69 @@
+# package created using:
+# http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+
+init <- function(){
+	# initialize rJava
+	library(rJava)
+	.jinit()
+	# add java implementation to classpath
+	.jaddClassPath(dir(paste(path.package("rCBA"), "/java/", sep=""), full.names=TRUE))
+	# add jar archives to classpath
+	jars <- list.files(paste(path.package("rCBA"), "/java/lib/", sep=""))
+	for(jar in jars){
+		if(!grepl("validation-api",jar)){
+			.jaddClassPath(paste(path.package("rCBA"), "/java/lib/", jar, sep=""))
+		}
+	}
+	# add configuration files to classpath
+	confs <- list.files(paste(path.package("rCBA"), "/java/conf/", sep=""))
+	for(conf in confs){
+		.jaddClassPath(paste(path.package("rCBA"), "/java/conf/", conf, sep=""))
+	}
+	.jclassPath()
+}
+
+#' A Pruning function
+#'
+#' @param train data.frame with training data
+#' @param rules data.frame with rules
+#' @param method pruning method m2cba(default)|m1cba|dcbrcba
+#' @return data.frame with pruned rules
+#' @export
+#' @examples
+#' library("arules")
+#' library("rCBA")
+#' 
+#' train <- read.csv("./train.csv",header=TRUE) # read data
+#' 
+#' txns <- as(train,"transactions") # convert
+#' rules <- apriori(txns, parameter = list(confidence = 0.1, support= 0.1, minlen=1, maxlen=5)) # rule mining
+#' rules <- subset( rules, subset = rhs %pin% "y=") # filter
+#' rulesFrame <- as(rules,"data.frame") # convert
+#' 
+#' print(nrow(rulesFrame))
+#' prunedRulesFrame <- pruning(trainData, rulesFrame, method="m2cba")
+#' print(nrow(prunedRulesFrame))
+pruning <- function(train, rules, method="m2cba"){
+	# init java
+	init()
+	# init interface
+	jPruning <- J("cz.jkuchar.rcba.r.RSpring")$initializePruning()
+	# set column names
+	.jcall(jPruning, , "setColumns", .jarray(colnames(train)))
+	# add rules
+	for (i in 1:nrow(rules)){
+		.jcall(jPruning, , "addRule", as.character(rules[i,]$rules), as.numeric(rules[i,]$confidence), as.numeric(rules[i,]$support))
+	}
+	# add train items
+	for(i in 1:nrow(train)){
+		.jcall(jPruning, , "addItem", as.character(unname(unlist(train[i,]))))
+	}
+	# perform pruning
+	jPruned <- .jcall(jPruning, "[Lcz/jkuchar/rcba/rules/Rule;", "prune", as.character(method))
+	# build dataframe
+	pruned <- data.frame(rules=rep("", 0), support=rep(0.0, 0), confidence=rep(0.0, 0), lift=rep(0.0, 0), stringsAsFactors=FALSE) 
+	for(jRule in jPruned){
+		pruned[nrow(pruned) + 1,] <- c(.jcall(jRule, "S", "getText"), .jcall(jRule, "D", "getSupport"), .jcall(jRule, "D", "getConfidence"), 0.0)
+	}
+	pruned
+}
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # rCBA
+
+CBA classifier for R
+
+## Installation
+
+Prerequisites:
+
+- Java 8
+- R packages - devtools, rJava
+
+### R dependencies installation
+```R
+install.packages(c("devtools","rJava"),dependencies=TRUE, repos="http://mirrors.nic.cz/R/")
+```
+
+### Reconfiguration of Java in R
+```bash
+sudo R CMD javareconf
+```
+
+### rCBA installation
+```R
+library("devtools")
+devtools::install_github("jaroslav-kuchar/rCBA")
+```
+
+## Usage
+
+```R
+library("arules")
+library("rCBA")
+
+train <- read.csv("./train.csv",header=TRUE) # read data
+
+txns <- as(train,"transactions") # convert
+rules <- apriori(txns, parameter = list(confidence = 0.1, support= 0.1, minlen=1, maxlen=5)) # rule mining
+rules <- subset( rules, subset = rhs %pin% "y=") # filter
+rulesFrame <- as(rules,"data.frame") # convert
+
+print(nrow(rulesFrame))
+prunedRulesFrame <- pruning(trainData, rulesFrame, method="m2cba") # m2cba(default)|m1cba|dcbrcba
+print(nrow(prunedRulesFrame))
+```
+## Contributors
+
+- Jaroslav Kuchař (https://github.com/jaroslav-kuchar)
+
+## Licence
+
+Apache License Version 2.0
